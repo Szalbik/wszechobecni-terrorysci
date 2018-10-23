b3f5db0a5fae0a07d8ba95c4f6f1f6a301025380
Damian Szalbierz <szalbierz.d.k@gmail.com>
2018-10-24 00:15:58 +0200

    fix labels

--- main.py
+++ main.py
@@ -7,9 +7,10 @@ liczbaOsob = 10**4
 prawdSpotk = 0.1
 
 m = {}
-rowno_dwa_spotkania = set()
-dwa_spotkania = set()
-trzy_spotkania = set()
+liczba_podejrzanych_osob = set()
+jedno_spotkanie = 0
+dwa_spotkania = 0
+trzy_spotkania = 0
 pary_osob_dni = 0
 
 for dzien in range(liczbaDni):
@@ -23,13 +24,16 @@ for dzien in range(liczbaDni):
             m[pair] = m.get(pair, 0)+1
 
 for k, v in m.items():
+    if v == 1:
+        jedno_spotkanie += 1
+    if v== 2:
+        dwa_spotkania +=  1
     if v >= 2:
         pary_osob_dni = pary_osob_dni + (v*(v-1)/2)
-        dwa_spotkania.add(k[0])
-        dwa_spotkania.add(k[1])
-    if v >= 3:
-        trzy_spotkania.add(k[0])
-        trzy_spotkania.add(k[1])
+        liczba_podejrzanych_osob.add(k[0])
+        liczba_podejrzanych_osob.add(k[1])
+    if v == 3:
+        trzy_spotkania += 1
 
 print("Liczba dni: "+str(liczbaDni))
 print("Liczba hoteli: "+str(liczbaHoteli))
@@ -37,6 +41,8 @@ print("Liczba osob: "+str(liczbaOsob))
 print("Prawdopodobieństwo spotkania: "+str(prawdSpotk))
 print("----------------------------------------")
 print("Pary które spotkały się minimum raz: "+str(len(m)))
-print("Pary które spotkały się minimum dwa razy: "+str(len(dwa_spotkania)))
-print("Pary które spotkały się minimum trzy razy: "+str(len(trzy_spotkania)))
-print("Liczba podejrzanych par 'osób i dni': "+str(pary_osob_dni))
+print("Pary które spotkały się raz: "+str(jedno_spotkanie))
+print("Pary które spotkały się dwa razy: "+str((dwa_spotkania)))
+print("Pary które spotkały się trzy razy: "+str((trzy_spotkania)))
+print("Liczba podejrzanych osób: "+str(len(liczba_podejrzanych_osob)))
+print("Liczba podejrzanych par 'osób i dni': "+str(int(pary_osob_dni)))
