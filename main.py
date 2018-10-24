0d02077b5df68559e59f73b600ad2a256c8e6981
Damian Szalbierz <szalbierz.d.k@gmail.com>
2018-10-24 17:03:39 +0200

    rename varriables for better understanding

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ liczbaHoteli = 10**2
 liczbaOsob = 10**4
 prawdSpotk = 0.1
 
-m = {}
+podejrzane_pary = {}
 liczba_podejrzanych_osob = set()
 jedno_spotkanie = 0
 dwa_spotkania = 0
@@ -14,16 +14,16 @@ trzy_spotkania = 0
 pary_osob_dni = 0
 
 for dzien in range(liczbaDni):
-    hs = {}
+    h = {}
     for osoba in range(liczbaOsob):
         if random.random() < prawdSpotk:
             hotel = random.randint(0, liczbaHoteli)
-            hs[hotel] = hs.get(hotel, []) + [osoba]
-    for v in hs.values():
+            h[hotel] = h.get(hotel, []) + [osoba]
+    for v in h.values():
         for pair in list(itertools.combinations(v, 2)):
-            m[pair] = m.get(pair, 0)+1
+            podejrzane_pary[pair] = podejrzane_pary.get(pair, 0)+1
 
-for k, v in m.items():
+for k, v in podejrzane_pary.items():
     if v == 1:
         jedno_spotkanie += 1
     if v== 2:
@@ -40,7 +40,7 @@ print("Liczba hoteli: "+str(liczbaHoteli))
 print("Liczba osob: "+str(liczbaOsob))
 print("Prawdopodobieństwo spotkania: "+str(prawdSpotk))
 print("----------------------------------------")
-print("Pary które spotkały się minimum raz: "+str(len(m)))
+print("Pary które spotkały się minimum raz: "+str(len(podejrzane_pary)))
 print("Pary które spotkały się raz: "+str(jedno_spotkanie))
 print("Pary które spotkały się dwa razy: "+str((dwa_spotkania)))
 print("Pary które spotkały się trzy razy: "+str((trzy_spotkania)))
